026a7390963f682f7c1339e0a912fad97d876171
Andrés Riquelme <andresfranco.rs@gmail.com>
2020-08-06 23:32:44 -0400

    test test

--- docs/index.md
+++ docs/index.md
@@ -17,10 +17,5 @@ The game itself is a chaos machine, sometimes its a 30 minutes long [battle roya
 
 Anyway, since this wiki is awfully incomplete, you should add your own stuff to it. [Get to work](So-close-to-impossible-that-it-might-as-well-not-even-exist.md) u moron, and learn the [Space Law](Space-Law.md).
 
-{% for server in get_server_data() %}
-Name: {{ server["ServerName"] }}
-Players: {{ server["PlayerCount"] }}
-Port: {{ server["ServerPort"] }}
-</br>
-{% endfor %}
+{{ get_server_data }}
 
--- docs/main.py
+++ docs/main.py
@@ -4,11 +4,47 @@ from pprint import pprint
 
 url = "https://api.unitystation.org/serverlist"
 
+shiet = \
+    """
+<div class="card" style="width: 18rem;">
+  <div class="card-header">
+    {}
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">IP: {} Port: {}</li>
+    <li class="list-group-item">Map: {}</li>
+    <li class="list-group-item">Game mode: {}</li>
+    <li class="list-group-item">Players: {}</li>
+  </ul>
+</div>
+<br>
+"""
+
 
 @env.macro
 def get_server_data():
     response = json.loads(requests.get(url).text)
-    return response
+    # pprint(response)
+
+    html = build_html(response)
+
+    return html
+
+
+def build_html(api_response):
+    html = ""
+
+    for server in api_response["servers"]:
+        name = server["ServerName"]
+        ip = server["ServerIP"]
+        port = server["ServerPort"]
+        map = server["CurrentMap"]
+        gamemode = server["GameMode"]
+        players = server["PlayerCount"]
+
+        html += shiet.format(name, ip, port, map, gamemode, players)
+
+    return html
 
 
 if __name__ == "__main__":
